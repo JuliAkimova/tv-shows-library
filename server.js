67cd1161a6ca0f42e97d0ec7942f7b037a4aa320
Julia Akimova <juliakimova.j@gmail.com>
2019-02-07 11:40:24 +0200

    server listening

--- server.js
+++ server.js
@@ -3,3 +3,12 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
 const app = express();
+
+
+/* Port listening */
+// Use port 3000 unless there exists a preconfigured port
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server started on post: ${port}.`);
+});
