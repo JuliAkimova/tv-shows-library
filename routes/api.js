0e0e7ea869c16ba4a8f8a0c3215c3541374bde05
Julia Akimova <juliakimova.j@gmail.com>
2019-02-13 19:06:31 +0200

    POST request to api/shows route

--- routes/api.js
+++ routes/api.js
@@ -27,3 +27,43 @@ const Season = require('../models/season');
 //require episode model
 const Episode = require('../models/episode');
 
+
+//@route POST api/shows
+//@desc Create a show
+//@access Public
+router.post('/', upload.single('posterImage'), (req, res, next) => {
+    const show = new Show({
+        _id: new mongoose.Types.ObjectId,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        dateOfStart: req.body.dateOfStart,
+        posterImage: req.file.path,
+        longDescrition: req.body.longDescrition,
+        shortDescrition: req.body.shortDescrition,
+        priority: req.body.priority,
+        videoFragmentURL: req.body.videoFragmentURL
+    });
+    show.save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Show created successfully',
+                createdShow: {
+                    _id: result.id,
+                    title: result.title,
+                    subtitle: result.subtitle,
+                    dateOfStart: result.dateOfStart,
+                    longDescrition: result.longDescrition,
+                    shortDescrition: result.shortDescrition,
+                    priority: result.priority,
+                    videoFragmentURL: result.videoFragmentURL,
+                    request: {
+                        type: 'GET',
+                        url: `http://127.0.0.1:3000/api/shows/${result._id}`
+                    } 
+                } 
+            });
+        })
+        .catch(err => res.status(500).json({error: err}))
+});
+
+module.exports = router;
