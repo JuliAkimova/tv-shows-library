57eb1d5a5746570e1b05b7894f420252fee0f667
Julia Akimova <juliakimova.j@gmail.com>
2019-02-14 21:32:34 +0200

    added password encryption

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 //Import admin model
 const Admin = require('../models/admin');
@@ -9,20 +10,26 @@ const Admin = require('../models/admin');
 //@desc Create new administrator
 //@access Public
 router.post('/signup', (req, res, next) => {
-    const admin = new Admin({
-            _id: new mongoose.Types.ObjectId(),
-            email: req.body.email,
-            password: req.body.password
-        });
-        admin
-            .save()
-            .then(result => {
-                console.log(result);
-                res.status(201).json({
-                    message: 'Admin created!'
+    bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if(err) {
+            return res.status(500).json({ error: err });
+        } else {
+            const admin = new Admin({
+                _id: new mongoose.Types.ObjectId(),
+                email: req.body.email,
+                password: hash
                 });
-            })
-            .catch(err => res.status(500).json({error: err}))
+            admin
+                .save()
+                .then(result => {
+                    console.log(result);
+                    res.status(201).json({
+                        message: 'Admin created!'
+                    });
+                })
+        .catch(err => res.status(500).json({error: err}))    
+        }
+    })
 });
 
 //@route POST admin/signin
