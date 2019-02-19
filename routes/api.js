3081877f89ff4d4d84c13e9180ee84d61a0c5f95
Julia Akimova <juliakimova.j@gmail.com>
2019-02-19 10:25:32 +0200

    Revert "added api/shows route controller"
    This reverts commit 58b24d5c79c6ed72d0ae0a70096356403cbe1026.

--- routes/api.js
+++ routes/api.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
 const multer = require('multer');
 const checkAuth = require('./check-auth');
-
-// Import Show controller
-const apiController = require('../controllers/api');
 
 //store images in dababase
 const storage = multer.diskStorage({
@@ -23,29 +21,138 @@ const upload = multer({
     }
 });
 
-//@route GET api/shows
-//@desc Get all shows
-//@access Public
-router.get('/', apiController.get_all_shows);
+//require show model
+const Show = require('../models/show');
+//require season model
+const Season = require('../models/season');
+//require episode model
+const Episode = require('../models/episode');
 
-//@route GET api/shows/:id
-//@desc Get show with specific id
-//@access Public
-router.get('/:showId', apiController.get_certain_show);
 
 //@route POST api/shows
 //@desc Create a show
 //@access Private
-router.post('/', checkAuth, upload.single('posterImage'), apiController.create_show);
+router.post('/', checkAuth, upload.single('posterImage'), (req, res, next) => {
+    const show = new Show({
+        _id: new mongoose.Types.ObjectId,
+        title: req.body.title,
+        subtitle: req.body.subtitle,
+        dateOfStart: req.body.dateOfStart,
+        posterImage: req.file.path,
+        longDescrition: req.body.longDescrition,
+        shortDescrition: req.body.shortDescrition,
+        priority: req.body.priority,
+        videoFragmentURL: req.body.videoFragmentURL
+    });
+    show.save()
+        .then(result => {
+            res.status(201).json({
+                message: 'Show created successfully',
+                createdShow: {
+                    _id: result.id,
+                    title: result.title,
+                    subtitle: result.subtitle,
+                    dateOfStart: result.dateOfStart,
+                    longDescrition: result.longDescrition,
+                    shortDescrition: result.shortDescrition,
+                    priority: result.priority,
+                    videoFragmentURL: result.videoFragmentURL,
+                    request: {
+                        type: 'GET',
+                        url: `http://127.0.0.1:3000/api/shows/${result._id}`
+                    } 
+                } 
+            });
+        })
+        .catch(err => res.status(500).json({error: err}))
+});
 
-//@route PATCH api/shows/:id
+//@route GET api/shows
+//@desc Get all shows
+//@access Public
+router.get('/', (req, res, next) => {
+    Show.find({})
+        //.select('_id title subtitle...')
+        .then(docs => {
+            const response = {
+                count: docs.length,
+                shows: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        title: doc.title,
+                        subtitle: doc.subtitle,
+                        dateOfStart: doc.dateOfStart,
+                        posterImage: doc.posterImage,
+                        longDescrition: doc.longDescrition,
+                        shortDescrition: doc.shortDescrition,
+                        priority: doc.priority,
+                        videoFragmentURL: doc.videoFragmentURL,
+                        request: {
+                            type: 'GET',
+                            url: `http://127.0.0.1:3000/api/shows/${doc._id}`
+                        }
+                    };
+                })
+
+            }; 
+            res.status(200).json(response);
+        })    
+        .catch(err => res.status(404).send(err)) 
+});
+
+//@route GET api/shows/:id
+//@desc Get show with specific id
+//@access Public
+router.get('/:showId', (req, res, next) => {
+    Show.findById(req.params.showId)
+        .then(doc => {
+            if(doc) {
+                res.status(200).json({
+                    show: doc,
+                    request: {
+                        type: 'GET',
+                        url: `http://127.0.0.1:3000/api/shows`
+                    }
+                })
+            } else {
+                res.status(404).json({ massage: 'No valid entry found for provided ID' })
+            }
+        })
+        .catch(err => res.status(500).json({ error: err }))
+});
+
+//@route PATCH api/items/:id
 //@desc Update a show
 //@access Private
-router.patch('/:showId', checkAuth, apiController.update_show);
+router.patch('/:showId', checkAuth, (req, res, next) => {
+    Show.findByIdAndUpdate({ _id: req.params.showId }, req.body)
+        .then(() => {
+            Show.findOne({ _id: req.params.showId })
+            .then(result => {
+                res.status(200).json({
+                    message: 'Show updated',
+                    title: result.title,
+                    request: {
+                        type: 'GET',
+                        url: `http://127.0.0.1:3000/api/shows/${req.params.showId}`
+                    }
+                })
+            })
+        .catch(err => res.status(500).json({ error: err }))    
+    });
+});
 
-//@route DELETE api/shows/:id
+//@route DELETE api/items/:id
 //@desc Delete a show
 //@access Private
-router.delete('/:showId', checkAuth, apiController.delete_show);
+router.delete('/:showId', checkAuth, (req, res, next) => {
+    Show.findByIdAndRemove({ _id: req.params.showId })
+        .then(() => {
+            res.status(200).json({
+                message: 'Show deleted'
+            })
+        })
+        .catch(err => res.status(500).json({ deleted: false, error: err }))
+});
 
 module.exports = router;
