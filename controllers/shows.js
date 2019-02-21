f3c2e10cd7b495eec64e025b74b0faffa162a34d
Julia Akimova <juliakimova.j@gmail.com>
2019-02-21 18:43:15 +0200

    added references to seasons and episode collections

--- controllers/shows.js
+++ controllers/shows.js
@@ -4,11 +4,74 @@ const mongoose = require('mongoose');
 const Show = require('../models/show');
 
 exports.get_all_shows = (req, res, next) => {
-    Show.find()
-    .then(docs => {
-        const response = {
-            shows: docs.map(doc => {
-                return {
+    Show
+        .find()
+        .sort({ priority: -1 })
+        .populate({
+            path: 'seasons',
+            populate: { path: 'episodes' }
+        }) 
+        .then(docs => {
+            const response = {
+                shows: docs.map(doc => {
+                    return {
+                        _id: doc._id,
+                        title: doc.title,
+                        subtitle: doc.subtitle,
+                        dateOfStart: doc.dateOfStart,
+                        posterImage: doc.posterImage,
+                        longDescrition: doc.longDescrition,
+                        shortDescrition: doc.shortDescrition,
+                        priority: doc.priority,
+                        videoFragmentURL: doc.videoFragmentURL,
+                        seasons: 
+                            doc.seasons.map(s => {
+                                return {
+                                    _id: s._id,
+                                    name: s.name,
+                                    number: s.number,
+                                    //relatedShow: s.relatedShow,
+                                    longDescription: s.longDescription,
+                                    shortDescrition: s.shortDescrition,
+                                    featuredImage: s.featuredImage,
+                                    videoFragmentURL: s.videoFragmentURL,
+                                    //rating: s.rating,
+                                    episodes:
+                                        s.episodes.map(e => {
+                                            return {
+                                                id: e._id,
+                                                number: e.number,
+                                                name: e.name,
+                                                //relatedShow: e.relatedShow,
+                                                //relatedSeason: e.relatedSeason 
+                                                longDescription: e.longDescription,
+                                                shortDescrition: e.shortDescrition,
+                                                featuredImage: e.featuredImage,
+                                                videoFragmentURL: e.videoFragmentURL,
+                                                //rating: e.rating
+                                            } 
+                                        })
+                                    }
+                            })
+                    }
+                })
+            };
+            res.status(200).json(response);
+        })    
+        .catch(err => res.status(404).json({ error: err }))     
+};
+
+exports.get_certain_show = (req, res, next) => {
+    Show
+        //.findOne({ title: req.query.title })
+        .findById(req.params.showId)
+        .populate({
+            path: 'seasons',
+            populate: { path: 'episodes' }
+        }) 
+        .then(doc => {
+            if(doc) {
+                res.status(200).json({
                     _id: doc._id,
                     title: doc.title,
                     subtitle: doc.subtitle,
@@ -18,30 +81,45 @@ exports.get_all_shows = (req, res, next) => {
                     shortDescrition: doc.shortDescrition,
                     priority: doc.priority,
                     videoFragmentURL: doc.videoFragmentURL,
-                };
-            })
-        }; 
-        res.status(200).json(response);
-    })    
-    .catch(err => res.status(404).json({ error: err })) 
-};
-
-exports.get_certain_show = (req, res, next) => {
-    Show.findById(req.params.showId)
-    .then(doc => {
-        if(doc) {
-            res.status(200).json({
-                show: doc
-            })
-        } else {
-            res.status(404).json({ massage: 'No valid entry found for provided ID' })
-        }
-    })
-    .catch(err => res.status(500).json({ error: err }))
+                    seasons:
+                        doc.seasons.map(s => {
+                            return {
+                                _id: s._id,
+                                name: s.name,
+                                number: s.number,
+                                //relatedShow: s.relatedShow,
+                                longDescription: s.longDescription,
+                                shortDescrition: s.shortDescrition,
+                                featuredImage: s.featuredImage,
+                                videoFragmentURL: s.videoFragmentURL,
+                                //rating: s.rating,
+                                episodes:
+                                    s.episodes.map(e => {
+                                        return {
+                                            id: e._id,
+                                            number: e.number,
+                                            name: e.name,
+                                            //relatedShow: e.relatedShow,
+                                            //relatedSeason: e.relatedSeason 
+                                            longDescription: e.longDescription,
+                                            shortDescrition: e.shortDescrition,
+                                            featuredImage: e.featuredImage,
+                                            videoFragmentURL: e.videoFragmentURL,
+                                            //rating: e.rating
+                                        }
+                                    })        
+                            }   
+                        })
+                })
+            } else {
+                res.status(404).json({ massage: 'No valid entry found for provided ID' })
+            }
+        })
+        .catch(err => res.status(500).json({ error: err }))  
 };
 
 exports.create_show = (req, res, next) => {
-    const show = new Show({
+    const newShow = new Show({
         _id: new mongoose.Types.ObjectId,
         title: req.body.title,
         subtitle: req.body.subtitle,
@@ -52,7 +130,8 @@ exports.create_show = (req, res, next) => {
         priority: req.body.priority,
         videoFragmentURL: req.body.videoFragmentURL
     });
-    show.save()
+    newShow
+        .save()
         .then(result => {
             res.status(201).json({
                 message: 'Show created successfully',
@@ -63,21 +142,24 @@ exports.create_show = (req, res, next) => {
 };
 
 exports.update_show = (req, res, next) => {
-    Show.findByIdAndUpdate({ _id: req.params.showId }, req.body)
+    Show
+        .findByIdAndUpdate({ _id: req.params.showId }, req.body)
         .then(() => {
-            Show.findOne({ _id: req.params.showId })
-            .then(result => {
-                res.status(200).json({
-                    message: 'Show updated',
-                    updatedShow: result
+            Show
+                .findOne({ _id: req.params.showId })
+                .then(result => {
+                    res.status(200).json({
+                        message: 'Show updated',
+                        updatedShow: result
+                    })
                 })
-            })
         .catch(err => res.status(500).json({ updated: false, error: err }))    
     }); 
 };
 
 exports.delete_show = (req, res, next) => {
-    Show.findByIdAndRemove({ _id: req.params.showId })
+    Show
+        .findByIdAndRemove({ _id: req.params.showId })
         .then(() => {
             res.status(200).json({
                 message: 'Show deleted'
